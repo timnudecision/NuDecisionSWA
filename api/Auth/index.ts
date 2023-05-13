f0fc7120554d9fdfd85597ce95901044fe3e69d3
Tim Johnston <tim@nudecision.com>
2023-05-13 16:06:21 -0500

    Change role names to Guest,User,Admin

--- api/Auth/index.ts
+++ api/Auth/index.ts
@@ -4,19 +4,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   //context.log('HTTP trigger function processed a request.');
   const name1 = req.query.name;
 
-  // //Roles -'Reader','Editor','Administrator'
+  // Roles -'Guest','User','Admin'
   const RoleMapping = [
-    { user: "admin@sha2013.onmicrosoft.com", role: ["Administrator"] },
-    { user: "varsha@no2labs.onmicrosoft.com", role: ["Reader"] },
-    { user: "tim@nudecision.com", role: ["Administrator"] },
-    { user: "tim@mtjohnston.net", role: ["Reader"] },
+    { user: "admin@sha2013.onmicrosoft.com", role: ["Admin"] },
+    { user: "varsha@no2labs.onmicrosoft.com", role: ["User"] },
+    { user: "tim@nudecision.com", role: ["Admin"] },
+    { user: "tim@mtjohnston.net", role: ["User"] },
   ];
 
   const userroles = RoleMapping.filter(function (rl) {
     return rl.user.toLowerCase() == name1.toLowerCase();
   });
 
-  const responseMessage = userroles.length > 0 ? userroles[0].role : ["no roles assigned"];
+  const responseMessage = userroles.length > 0 ? userroles[0].role : []; // no authorized roles = []
 
   //const responseMessage="found1 "+name;
   context.res = {
